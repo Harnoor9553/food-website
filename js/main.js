6e57c21764049215331362654774ae3c71962cfb
harry-kaur <harnoorkaur16165@gmail.com>
2026-02-19 00:51:18 +0530

    getting the hang of counter hmm

--- js/main.js
+++ js/main.js
@@ -24,4 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     counter("count1", 0, 1287, 3000);
+    counter("count2", 100, 5786, 2500);
+    counter("count3", 0, 1440, 3000);
+    counter("count4", 0, 7110, 3000);
 });
